8da0cf6fc5b69949866ca343bb61586df58a7b5c
ericolivrib <ribericoliveira@protonmail.ch>
2022-02-05 18:23:08 -0300

    status alterado para o tipo Status

--- src/main/java/bolicho/model/Pedido.java
+++ src/main/java/bolicho/model/Pedido.java
@@ -21,5 +21,5 @@ public class Pedido {
     private LocalDate dataEntrega;
     private LocalDate dataFinalizado;
     private BigDecimal total;
-    private String status;
+    private Status status;
 }
